3048593e646de212d2a52f4314e1df0e48a28e72
Argonozo <elmecaniqueador@gmail.com>
2024-12-17 04:29:21 -0300

    OTRA VERSION ESTABLEEE

--- app/travel/routes.py
+++ app/travel/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, jsonify,
 from flask_login import login_required, current_user
 from app.travel.models import Destination, Package, Reservation
 from app.travel.forms import ReservationForm
+from datetime import timedelta
 from app import db
 
 
@@ -58,20 +59,57 @@ def get_destination_description(destination_id):
     destination = Destination.query.get_or_404(destination_id)
     return jsonify(description=destination.description)
 
+
+
+@travel.route('/reserve_package', methods=['POST'])
+@login_required
+def reserve_package():
+    package_id = request.form.get('package')
+    travel_date = request.form.get('travel_date_pack')
+    if package_id and travel_date:
+        new_reservation = Reservation(
+            user_id=current_user.id,
+            package_id=package_id,
+            travel_date=travel_date
+        )
+        db.session.add(new_reservation)
+        db.session.commit()
+        flash('¡Reserva de paquete creada correctamente!', 'success')
+    else:
+        flash('Por favor selecciona un paquete y una fecha válida.', 'danger')
+    return redirect(url_for('travel.reserve'))
+
+
+
+
+@travel.route('/get_package_dates/<int:package_id>', methods=['GET'])
+def get_package_dates(package_id):
+    package = Package.query.get(package_id)
+    if package:
+        start_date = package.available_from
+        end_date = package.available_to
+        delta = (end_date - start_date).days
+        dates = [(start_date + timedelta(days=i)).strftime('%Y-%m-%d') for i in range(delta + 1)]
+        return {'dates': dates}, 200
+    return {'dates': []}, 404
+
+
+
+
 @travel.route('/get_package_description/<int:package_id>')
 def get_package_description(package_id):
     package = Package.query.get_or_404(package_id)
     return jsonify(description=package.description)
 
+
 @travel.route('/packages')
-@login_required
 def packages():
     # Consultar todos los paquetes desde la base de datos
     packages = Package.query.all()
     return render_template('travel/packages.html', packages=packages)
 
+
 @travel.route('/destinations')
-@login_required
 def destinations():
     # Consultar todos los destinos desde la base de datos
     destinations = Destination.query.all()
